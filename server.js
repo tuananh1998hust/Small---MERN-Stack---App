258136e58a63da3214730ccbc0ea3bfbdf0bb3aa
tuananh1998hust <tuananh1998.hust@gmail.com>
2019-04-02 21:40:23 +0700

    add project structure

---
+++ server.js
@@ -0,0 +1,21 @@
+// config env
+require('dotenv').config();
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const app = express();
+
+const port = process.env.PORT || 5000;
+
+// Middleware
+app.use(bodyParser.json());
+
+// Connect MongoDB
+mongoose
+  .connect(process.env.MONGO_URI, { useNewUrlParser: true })
+  .then(() => console.log('MongoDB is connected'))
+  .catch(err => console.log(err));
+
+app.listen(port, () => console.log(`server is starting on port ${port}`));
